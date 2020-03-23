f9ec6441b1bb8b1a984f061cd59fb0d7d8a98441
LenWinkler <lenwinkler86@gmail.com>
2020-03-23 16:44:25 -0400

    added ram_read and ram_write to CPU class

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -11,6 +11,12 @@ class CPU:
         self.reg = [0] * 8
         self.pc = 0
 
+    def ram_read(self, address):
+        return self.ram[address]
+
+    def ram_write(self, address, value):
+        self.ram[address] = value
+
     def load(self):
         """Load a program into memory."""
 
